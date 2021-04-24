fde75ee087ff867ef5df1e7619d79a7c587308c1
temur <temur1464@gmail.com>
2021-04-24 11:29:41 +0500

    python

---
+++ hello-world.py
@@ -0,0 +1,138 @@
+#yosh=int(input("Yoshinggiz nechida "))
+#if yosh<=4:
+#    print("Kirish bepul")
+#elif yosh<12:
+#    print("Kirish 5000 so'm")
+#else:
+#    print("Kirish 10000 so'm")
+#yosh=int(input("Yoshinggiz nechida "))
+#if yosh<=4:
+#    narh=0
+#elif yosh<=12:
+#    narh=5000
+#elif yosh<=18:
+#    narh=8000
+#else:
+#    narh=10000
+#print(f"Kirish {narh} so'm")    
+
+#kun=input("Bugun nima kun ")
+#if kun.lower()=="yakshanba" or kun.lower()=="shanba":
+ #   print("Dam olish kuni")
+#else:
+#    print("Ish kuni")
+    
+#kun=input("Bugun nima kun ")
+#harorat=float(input("Harorat nechi "))    
+#if kun.lower()=="yakshanba" and harorat>=30:
+#    print("Chomilishga ketdik")
+#else:
+# print("Uyda dam olamiz")    
+#elif kun.lower()=='yakshanba' and harorat<30:
+   # print("Uyda dam olamiz!")
+    
+#kun =input("bugun kun nima?")
+#harorat=float(input("Harorat qanday?"))
+#if (kun.lower()=="yakshanba" or kun.lower()=="shanba") and harorat>=30:  
+#    print("Cho'milishga ketdik")
+#elif (kun.lower()=="yakshanba" or kun.lower()=="shanba") and harorat<30: 
+#    print("Uyda dam olamiz")
+#narh=12000
+#choy=1
+#salad=0
+#if choy and salad:
+#    narh=narh+10000
+#if choy or salad:
+#    narh=narh+5000
+#print("jami narh",narh)    
+    
+    
+#narh=15000
+#salad=1
+#choy=0
+#aroq=0
+#asarti=1
+#if salad:
+#    print("Mijoz salad oldi")
+#    narh=narh+5000
+#if choy:
+#    print("Mijoz choy oldi")
+#    narh=narh+5000
+#if aroq:
+#    print("Mijoz aroq oldi")
+#    narh=narh+5000
+#if asarti:
+#    print("Mijoz asarti oldi")
+#    narh=narh+5000
+#print("Jami hisob",narh)    
+    
+#ovqat=["Manti","chuchvara","osh"]
+#"osh" in ovqat   
+
+#menu=["osh","manti","kabob","barak"]
+#ovqat=input("Nima ovqat yeysiz?\n>>> ")
+#if ovqat.lower() in menu:
+#    print("Buyurtma qabul qilindi")
+#else:
+#    print("Bizda bu ovqat yoq")
+    
+    
+#menu=["osh","manti","kabob","shorva","baliq"]
+#buyurtmalar=[]
+#for ovqat in buyurtmalar:
+ #   if ovqat in menu:
+#        print(f"Menuda {ovqat} bor")
+ #   else:
+#        print(f"Menuda {ovqat} yo'q")
+#else:
+#    print("Savatchanggiz bo'sh")
+     
+#son=int(input(" Son kiriting >>> "))
+#if son/2==float():
+#    print(f"{son} bu toq son")
+#   
+#else:
+#     print(f"{son } bu son juft son")
+#son=float(input("Juft son kiriting?"))
+#if son%2:
+#     print("Bu juft son emas")
+#else:
+#     print("Raxmat")
+    
+yosh=int(input("Yoshinggizni kiriting >>> "))
+if yosh<=4:
+    narh=0
+elif yosh<=18:
+    narh=10000
+elif yosh>=18:
+    narh=20000
+if yosh>=60:
+    narh=0
+        
+print(f"Sizga kirish {narh} so'm")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
